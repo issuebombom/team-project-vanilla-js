04fc1cb173624ba711f109c591a427a4a5849bb2
givemeachocolate <132573959+givemeachocolate@users.noreply.github.com>
2023-06-06 11:01:10 +0900

    Delete detail.js

--- detail.js
+++
@@ -1,9 +0,0 @@
-const main = document.querySelector("#main")
-const detail = document.querySelector("#detail")
-
-function begin(){
-    main.style.display = "none"
-    detail.style.display = "block"
-}
-
-//${info.id}
