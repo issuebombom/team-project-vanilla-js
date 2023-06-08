e0ddb0b12c6fd663088cb921169aa4ac11681661
Juseung Kim <95juseung@gmail.com>
2023-06-08 15:42:49 +0900

    chore: minor

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 // 영화 목록 데이터 가져오기
 const getMovies = async () => {
   const options = {
-    method: "GET",
+    method: 'GET',
     headers: {
-      accept: "application/json",
+      accept: 'application/json',
       Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOTM2NWEzNmZhNmQwMmFlYTFmMzEyZjkwNmQzMTJlZCIsInN1YiI6IjY0NzViNGJkOTYzODY0MDExODQ4MDRlMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Kof3JjFhDwtfeYITdsJhMA4XedswNta1T8HVSunc7Sw",
+        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiOTM2NWEzNmZhNmQwMmFlYTFmMzEyZjkwNmQzMTJlZCIsInN1YiI6IjY0NzViNGJkOTYzODY0MDExODQ4MDRlMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Kof3JjFhDwtfeYITdsJhMA4XedswNta1T8HVSunc7Sw',
     },
   };
   const response = await fetch(
-    "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+    'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1',
     options
   );
   const data = await response.json();
@@ -19,7 +19,7 @@ const getMovies = async () => {
 
 // 영화 필터링 함수(영화 타이틀을 대소문자 상관없이 검색 키워드가 타이틀의 포함유무 확인)
 const filteringMovies = (movies, keyword) => {
-  const reg = new RegExp(keyword, "i");
+  const reg = new RegExp(keyword, 'i');
   const filteredMovies = movies.filter((movieInfo) =>
     reg.test(movieInfo.title)
   );
@@ -29,11 +29,11 @@ const filteringMovies = (movies, keyword) => {
 // 검색 수행
 const search = async () => {
   // 키워드 가져오기
-  const searchInput = document.getElementById("searchinput");
+  const searchInput = document.getElementById('searchinput');
   const keyword = searchInput.value.trim();
 
   // 검색 입력값이 비어있지 않은 경우 필터링 영화 카드 표시
-  if (keyword !== "") {
+  if (keyword !== '') {
     const movies = await getMovies(); // 영화 정보 가져오기
     const filteredMovies = filteringMovies(movies, keyword); // 필터링 결과 리턴
     createCards(filteredMovies); // 필터링된 영화 카드 생성
@@ -46,10 +46,10 @@ const search = async () => {
 
 // 영화 카드 생성
 const createCards = (movies) => {
-  const cardBox = document.querySelector(".card-box");
-  const modalContent = document.querySelector(".modal-content");
-  cardBox.innerHTML = ""; // 카드 생성 전 비워주기
-  modalContent.innerHTML = "";
+  const cardBox = document.querySelector('.card-box');
+  const modalContent = document.querySelector('.modal-content');
+  cardBox.innerHTML = ''; // 카드 생성 전 비워주기
+  modalContent.innerHTML = '';
 
   movies.forEach((movieInfo) => {
     // 영화 정보 HTML 생성
@@ -67,8 +67,8 @@ const createCards = (movies) => {
 
 // 영화 카드를 클릭했을 때 모달 창 열기
 const openModal = (movies, movieId) => {
-  const modal = document.getElementById("modal");
-  const modalContent = document.querySelector(".modal-content");
+  const modal = document.getElementById('modal');
+  const modalContent = document.querySelector('.modal-content');
   const movieInfo = movies.find((movie) => movie.id === Number(movieId));
 
   // 모달 내용 업데이트
@@ -92,18 +92,18 @@ const openModal = (movies, movieId) => {
                               </form>
                               `;
 
-  // 리뷰 뿌리기
+  // 리뷰 뿌리기F
   spreadReview(movieId);
 
   // 모달 창 열기
-  modal.style.display = "block";
-  document.body.style.overflow = "hidden";
+  modal.style.display = 'block';
+  document.body.style.overflow = 'hidden';
 
   // 모달 창 닫기
-  const closeModal = document.getElementById("close-modal");
-  closeModal.addEventListener("click", () => {
-    modal.style.display = "none";
-    document.body.style.overflow = "auto";
+  const closeModal = document.getElementById('close-modal');
+  closeModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+    document.body.style.overflow = 'auto';
   });
   // 포스팅 핸들러 등록
   postHandler(movieId);
@@ -121,9 +121,9 @@ spreadMovies();
 
 // [이벤트] 세부정보 모달창으로 띄우기 이벤트
 const modalHandler = (movies) => {
-  const cardBox = document.querySelector(".card-box");
-  cardBox.addEventListener("click", (event) => {
-    if (event.target.tagName === "IMG") {
+  const cardBox = document.querySelector('.card-box');
+  cardBox.addEventListener('click', (event) => {
+    if (event.target.tagName === 'IMG') {
       const movieId = event.target.alt;
       openModal(movies, movieId);
     }
@@ -133,43 +133,43 @@ const modalHandler = (movies) => {
 // [이벤트] 검색 이벤트
 // --------- form 태그를 이용해서 엔터키와 클릭 이벤트를 하나의 핸들러로 담을 수 있음
 // 검색 버튼 클릭 이벤트
-const searchBtn = document.getElementById("searchbtn");
-searchBtn.addEventListener("click", () => {
+const searchBtn = document.getElementById('searchbtn');
+searchBtn.addEventListener('click', () => {
   search();
 });
 
 // 검색 입력에서 Enter 키를 누를 때 이벤트
-const searchInput = document.getElementById("searchinput");
-searchInput.addEventListener("keyup", (event) => {
-  if (event.key === "Enter") {
+const searchInput = document.getElementById('searchinput');
+searchInput.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
     search();
   }
 });
 
 // 리뷰 작성 핸들러
 const postHandler = (movieId) => {
-  const postForm = document.querySelector(".post-form");
-  postForm.addEventListener("submit", (event) => {
+  const postForm = document.querySelector('.post-form');
+  postForm.addEventListener('submit', (event) => {
     event.preventDefault();
     let result = [];
     for (tag of event.target) {
       result.push(tag.value);
-      tag.value = "";
+      tag.value = '';
     }
     let [nickname, password, comment] = result;
 
-    if (nickname === "" || nickname.includes(" ")) {
-      alert("공백을 제외한 닉네임을 입력해주세요.");
+    if (nickname === '' || nickname.includes(' ')) {
+      alert('공백을 제외한 닉네임을 입력해주세요.');
       return;
     }
 
-    if(password.length < 4) {
-      alert("패스워드를 4자리 이상 입력해주세요.");
+    if (password.length < 4) {
+      alert('패스워드를 4자리 이상 입력해주세요.');
       return;
     }
 
-    if(comment === "") {
-      alert("리뷰를 한 글자 이상 남겨주세요.")
+    if (comment === '') {
+      alert('리뷰를 한 글자 이상 남겨주세요.');
       return;
     }
 
@@ -192,7 +192,7 @@ const postReview = (movieId, nickname, password, comment) => {
 
 // 리뷰 뿌리기
 const spreadReview = (movieId) => {
-  const reviewBox = document.querySelector(".review-box");
+  const reviewBox = document.querySelector('.review-box');
 
   if (localStorage.getItem(movieId) === null) {
     reviewBox.innerHTML = `<h2 class = "nocomments>댓글이 없습니다.</h2>`;
@@ -209,5 +209,5 @@ const spreadReview = (movieId) => {
         <p class="comments">내용: ${comment}</p>                
       </div>`
     );
-  }, "");
+  }, '');
 };
